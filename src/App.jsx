82e0fd28a05d7e6e4e5bf38baa792936c8528fe2
Hamza Essolami <essolamih@gmail.com>
2026-09-02 14:47:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,6 @@ function App() {
         </a>
       </nav>
 
-      {console.log("hello")}
-
       <section className="hero" id="top">
         <div className="hero-copy">
           <div className="eyebrow">
